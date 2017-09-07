af5508c7d0c7f5083507ea8c6ebdb6737e42bb8c
NicholasSmilovic <nicholas_smilovic@hotmail.com>
2017-09-07 20:50:24 +0000

    folowing instructions but learned option objects

--- https_example.js
+++ https_example.js
@@ -8,12 +8,21 @@ var options = {
 
 var callback = function (response) {
   console.log('In response handler callback');
-  console.log("response: ", response);
+  //console.log(response)
+  response.on('data', function(chunk) {
+    console.log('[--CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 
 }
 
 
 console.log("I'm about to make a request");
+/*
+https.request = function (options, cb) {
+  return new ClientRequest(options, cb);
+}
+*/
 https.request(options, callback).end();
 
-console.log("I've made the request");
+console.log("I've made the request: \n")// https.request(options, callback));
